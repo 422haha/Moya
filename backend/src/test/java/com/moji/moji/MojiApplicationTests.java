0fbef859dc92cc9648891abe0a7dac0f7356a57e
서장원 <seojang980510@naver.com>
2024-09-02 17:50:07 +0900

    S11P21D202-148 <feat> be init

---
+++ backend/src/test/java/com/moji/moji/MojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moji.moji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
