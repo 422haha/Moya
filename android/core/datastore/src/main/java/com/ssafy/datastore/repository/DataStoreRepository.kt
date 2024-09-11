fe57200ba4858ecab443f963a190bc7387e2e60d
김성수 <skele1207@gmail.com>
2024-09-11 13:07:37 +0900

    Revert "Merge branch 'feature/android/datastore/S11P21D202-244' into 'master'"
    This reverts merge request !44

--- android/core/datastore/src/main/java/com/ssafy/datastore/repository/DataStoreRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.datastore.repository
-
-interface DataStoreRepository {
-    suspend fun saveAccessToken(token: String)
-
-    suspend fun deleteAccessToken()
-
-    suspend fun getAccessToken(): String?
-}
