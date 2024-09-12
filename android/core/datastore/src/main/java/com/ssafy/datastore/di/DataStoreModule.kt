cd510f6bc2421a97deb0e4f56c13c80471513a39
김성수 <skele1207@gmail.com>
2024-09-12 14:01:50 +0900

    fix(S11P21D202-273) : dependency 오류 수정

--- android/core/datastore/src/main/java/com/ssafy/datastore/di/DataStoreModule.kt
+++ android/core/datastore/src/main/java/com/ssafy/datastore/di/DataStoreModule.kt
@@ -5,8 +5,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
-import androidx.test.espresso.core.internal.deps.dagger.Module
-import androidx.test.espresso.core.internal.deps.dagger.Provides
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
