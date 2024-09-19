e8591b4009d4df02ff888ed401849fc8f6fbb964
choi-jihun <cjh0630@naver.com>
2024-09-20 02:10:23 +0900

    chore: 변수명 수정

--- android/app/src/main/java/com/ssafy/moya/Navigation.kt
+++ android/app/src/main/java/com/ssafy/moya/Navigation.kt
@@ -5,15 +5,15 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.ssafy.main.encyclopedia.EncycScreen
 import com.ssafy.main.explorelist.ExploreListScreen
 import com.ssafy.main.home.HomeScreen
+import com.ssafy.main.parkdetail.ParkDetailScreen
 import com.ssafy.main.parklist.ParkListScreen
 import com.ssafy.ui.screen.EncycDetailScreen
-import com.ssafy.ui.screen.EncycScreen
 import com.ssafy.ui.screen.ExploreDetailScreen
 import com.ssafy.ui.screen.ExploreStartScreen
 import com.ssafy.ui.screen.LoginScreen
-import com.ssafy.ui.screen.ParkDetailScreen
 import com.ssafy.ui.screen.UserProfileEditScreen
 
 @Composable
@@ -49,14 +49,14 @@ fun MainNavigation(
         }
         composable<ExploreDetail> {
             ExploreDetailScreen(onItemClicked = { itemId ->
-                navController.navigate(EncycDetail(itemId.toLong()))
+                navController.navigate(EncycDetail(itemId))
             }, onPop = {
                 navController.popBackStack()
             })
         }
         composable<ParkDetail> {
-            ParkDetailScreen(onItemClicked = { itemId ->
-                navController.navigate(EncycDetail(itemId.toLong()))
+            ParkDetailScreen(onNavigateToEncycDetail = { itemId ->
+                navController.navigate(EncycDetail(itemId))
             }, onPop = {
                 navController.popBackStack()
             }, onEnterExplore = {
@@ -64,8 +64,8 @@ fun MainNavigation(
             })
         }
         composable<Encyc> {
-            EncycScreen(onItemClicked = { itemId ->
-                navController.navigate(EncycDetail(itemId.toLong()))
+            EncycScreen(onNavigateToEncycDetail = { itemId ->
+                navController.navigate(EncycDetail(itemId))
             }, onPop = {
                 navController.popBackStack()
             })
