d9db34c5af8e4ebc0e1fa7a61ec08fa2cffad6be
김은섭 <subway@kumoh.ac.kr>
2024-09-20 16:52:35 +0900

    feat: utils 추가

---
+++ backend/src/main/java/com/e22e/moya/chatbot/Utils.java
@@ -0,0 +1,56 @@
+package com.e22e.moya.chatbot;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static dev.langchain4j.internal.Utils.getOrDefault;
+import static io.github.cdimascio.dotenv.DslKt.dotenv;
+
+public class Utils {
+
+    private static final Dotenv dotenv = Dotenv.load();
+//    public static final String OPENAI_API_KEY = getOrDefault(System.getenv("OPENAI_API_KEY"), "demo");
+    public static final String OPENAI_API_KEY = dotenv.get("OPENAI_API_KEY");
+
+    public static void startConversationWith(Assistant assistant) {
+        Logger log = LoggerFactory.getLogger(Assistant.class);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                log.info("==================================================");
+                log.info("User: ");
+                String userQuery = scanner.nextLine();
+                log.info("==================================================");
+
+                if ("exit".equalsIgnoreCase(userQuery)) {
+                    break;
+                }
+
+                String agentAnswer = assistant.answer(userQuery);
+                log.info("==================================================");
+                log.info("Assistant: " + agentAnswer);
+            }
+        }
+    }
+
+    public static PathMatcher glob(String glob) {
+        return FileSystems.getDefault().getPathMatcher("glob:" + glob);
+    }
+
+    public static Path toPath(String relativePath) {
+        try {
+            URL fileUrl = Utils.class.getClassLoader().getResource(relativePath);
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
