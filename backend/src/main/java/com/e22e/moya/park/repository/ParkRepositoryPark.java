5da8e950842db34a3609b44e77b89a238ef8e1bd
강미연 <422haha@naver.com>
2024-09-20 11:12:59 +0900

    S11P21D202-349 feat: PostGIS를 활용한 공원 조회 쿼리 추가

--- backend/src/main/java/com/e22e/moya/park/repository/ParkRepositoryPark.java
+++ backend/src/main/java/com/e22e/moya/park/repository/ParkRepositoryPark.java
@@ -1,10 +1,7 @@
 package com.e22e.moya.park.repository;
 
 import com.e22e.moya.common.entity.park.Park;
-import org.geolatte.geom.G2D;
-import org.geolatte.geom.Point;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.e22e.moya.common.entity.park.ParkPos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,28 +13,33 @@ import java.util.Optional;
 @Repository
 public interface ParkRepositoryPark extends JpaRepository<Park, Long> {
 
-    @Query(value = "SELECT p.* FROM park p " +
-        "JOIN park_pos pp ON p.id = pp.park_id " +
-        "ORDER BY ST_Distance(pp.pos, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) ASC " +
-        "LIMIT 1",
-        nativeQuery = true)
-    Optional<Park> findNearestPark(@Param("latitude") double latitude,
-        @Param("longitude") double longitude);
+    @Query(value =
+        "SELECT p.* FROM park p " +
+            "JOIN park_pos pp ON p.id = pp.park_id " +
+            "ORDER BY ST_Distance(CAST(pp.pos AS geography), ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography) ASC " +
+            "LIMIT 1", nativeQuery = true)
+    Optional<Park> findNearestPark(@Param("latitude") double latitude, @Param("longitude") double longitude);
 
     @Query(value =
-        "SELECT p.*, ST_Distance(pp.pos, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) AS distance "
-            +
+        "SELECT p.*, ST_Distance(CAST(pp.pos AS geography), ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography) AS distance " +
             "FROM park p " +
             "JOIN park_pos pp ON p.id = pp.park_id " +
             "ORDER BY distance ASC " +
-            "LIMIT :size OFFSET :page * :size",
-        nativeQuery = true)
+            "LIMIT :size OFFSET :page * :size", nativeQuery = true)
     List<Park> findParksWithDistance(@Param("latitude") double latitude,
         @Param("longitude") double longitude,
         @Param("page") int page, @Param("size") int size);
 
-    @Query("SELECT ST_Distance(e.pos, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) " +
-        "FROM Park p JOIN p.entrances e WHERE p.id = :parkId")
-    double calculateDistance(@Param("longitude") double longitude,
-        @Param("latitude") double latitude, @Param("parkId") Long parkId);
+    @Query(value = "SELECT ST_Distance(CAST(e.pos AS geography), " +
+        "ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography) " +
+        "FROM park_pos e WHERE e.park_id = :parkId", nativeQuery = true)
+    double calculateDistance(@Param("parkId") Long parkId, @Param("latitude") double latitude,
+        @Param("longitude") double longitude);
+
+    @Query(value = "SELECT * FROM park_pos e " +
+        "WHERE e.park_id = :parkId " +
+        "ORDER BY ST_Distance(CAST(e.pos AS geography), ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography) ASC " +
+        "LIMIT 1", nativeQuery = true)
+    Optional<ParkPos> findNearestEntrance(@Param("parkId") Long parkId,
+        @Param("latitude") double latitude, @Param("longitude") double longitude);
 }
