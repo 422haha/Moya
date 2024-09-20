6ace7b9759ae1db498f23031624980f707577315
김성수 <skele1207@gmail.com>
2024-09-20 16:21:09 +0900

    feat : 탐험 관련 api 추가

--- android/core/network/src/main/java/com/ssafy/network/api/ExplorationApi.kt
+++ android/core/network/src/main/java/com/ssafy/network/api/ExplorationApi.kt
@@ -1,6 +1,10 @@
 package com.ssafy.network.api
 
+import com.ssafy.model.CompletedQuest
 import com.ssafy.model.ExplorationData
+import com.ssafy.model.ExplorationInitialData
+import com.ssafy.model.ExplorationJournalRecent
+import com.ssafy.model.QuestList
 import com.ssafy.network.ResponseBody
 import com.ssafy.network.request.ExplorationEndRequestBody
 import retrofit2.Response
@@ -10,10 +14,14 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ExplorationApi {
+
+    @GET("/exploration/home")
+    fun getRecentExploration(): Response<ResponseBody<ExplorationJournalRecent>>
+
     @GET("/exploration/start/{parkId}")
     fun startExploration(
         @Path("parkId") parkId: Long,
-    ): Response<ResponseBody<ExplorationData>>
+    ): Response<ResponseBody<ExplorationInitialData>>
 
     @POST("/explorations/{explorationId}/end")
     fun endExploration(
@@ -30,5 +38,16 @@ interface ExplorationApi {
     @GET("/explorations/{explorationId}/quest/list")
     fun getQuestList(
         @Path("explorationId") explorationId: Long,
-    )
+    ): Response<ResponseBody<QuestList>>
+
+    @POST("/explorations/{explorationId}/quest/{questId}/complete")
+    fun completeQuest(
+        @Path("explorationId") explorationId: Long,
+        @Path("questId") questId: Long,
+    ): Response<ResponseBody<CompletedQuest>>
+
+    @GET("/explorations/load/{explorationId}")
+    fun getExplorationData(
+        @Path("explorationId") explorationId: Long,
+    ): Response<ResponseBody<ExplorationData>>
 }
--- android/core/network/src/main/java/com/ssafy/network/repository/ExplorationRepository.kt
+++ android/core/network/src/main/java/com/ssafy/network/repository/ExplorationRepository.kt
@@ -3,7 +3,7 @@ package com.ssafy.network.repository
 import com.ssafy.network.ApiResponse
 
 interface ExplorationRepository {
-    suspend fun startExploration(parkId: Long): ApiResponse<com.ssafy.model.ExplorationData>
+    suspend fun startExploration(parkId: Long): ApiResponse<com.ssafy.model.ExplorationInitialData>
 
 
 }
