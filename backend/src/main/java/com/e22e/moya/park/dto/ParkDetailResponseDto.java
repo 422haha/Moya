4d42948e808c547a37bdcd10538e0409caf6e5a9
강미연 <422haha@naver.com>
2024-09-20 11:52:15 +0900

    S11P21D202-349 feat: 공원 상세 응답 DTO 수정

--- backend/src/main/java/com/e22e/moya/park/dto/ParkDetailResponseDto.java
+++ backend/src/main/java/com/e22e/moya/park/dto/ParkDetailResponseDto.java
@@ -11,18 +11,13 @@ public class ParkDetailResponseDto {
     private Long parkId;
     private String name;
     private String description;
-    private double latitude;
-    private double longitude;
     private String imageUrl;
     private List<SpeciesDto> species;
 
-    public ParkDetailResponseDto(Long parkId, String name, String description, double latitude,
-        double longitude, String imageUrl, List<SpeciesDto> species) {
+    public ParkDetailResponseDto(Long parkId, String name, String description, String imageUrl, List<SpeciesDto> species) {
         this.parkId = parkId;
         this.name = name;
         this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
         this.imageUrl = imageUrl;
         this.species = species;
     }
