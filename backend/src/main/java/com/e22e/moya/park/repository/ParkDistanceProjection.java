5342233521a6fad38adc37bd667b1804e05ac08a
강미연 <422haha@naver.com>
2024-09-20 15:16:35 +0900

    S11P21D202-354 feat: 공원 거리 계산용 Projection 인터페이스 추가

---
+++ backend/src/main/java/com/e22e/moya/park/repository/ParkDistanceProjection.java
@@ -0,0 +1,12 @@
+package com.e22e.moya.park.repository;
+
+public interface ParkDistanceProjection {
+
+    Long getId();
+
+    String getName();
+
+    String getImageUrl();
+
+    Double getDistance();
+}
