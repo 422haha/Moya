8af3dafc2e0081c55d013c8be2438895c3496f0c
yeolife <yeo2507@naver.com>
2024-09-22 04:49:27 +0900

    Feat. 비동기 작업 및 분기처리

--- android/feat/ar/src/main/java/com/ssafy/ar/ARScreen.kt
+++ android/feat/ar/src/main/java/com/ssafy/ar/ARScreen.kt
@@ -1,12 +1,6 @@
 package com.ssafy.ar
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.os.Looper
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +13,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -31,41 +24,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import com.google.android.filament.Engine
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import com.google.android.gms.maps.model.LatLng
 import com.google.ar.core.Config
 import com.google.ar.core.Frame
-import com.google.ar.core.Plane
 import com.google.ar.core.TrackingFailureReason
-import com.google.ar.core.dependencies.f
-import com.ssafy.ar.ArData.NPCLocation
-import com.ssafy.ar.ArData.QuestStatus
-import com.ssafy.ar.dummy.npcs
+import com.ssafy.ar.data.QuestStatus
+import com.ssafy.ar.manager.ARLocationManager
 import com.ssafy.ar.dummy.scriptNode
+import com.ssafy.ar.manager.ARNodeManager
+import com.ssafy.ar.util.MultiplePermissionsHandler
+import com.ssafy.ar.util.QuestDialog
 import io.github.sceneview.ar.ARScene
-import io.github.sceneview.ar.arcore.createAnchorOrNull
-import io.github.sceneview.ar.arcore.getUpdatedPlanes
 import io.github.sceneview.ar.node.AnchorNode
 import io.github.sceneview.ar.rememberARCameraNode
-import io.github.sceneview.loaders.MaterialLoader
-import io.github.sceneview.loaders.ModelLoader
 import io.github.sceneview.model.ModelInstance
 import io.github.sceneview.node.ImageNode
 import io.github.sceneview.node.ModelNode
-import io.github.sceneview.node.Node
 import io.github.sceneview.rememberCollisionSystem
 import io.github.sceneview.rememberEngine
 import io.github.sceneview.rememberMaterialLoader
@@ -73,27 +53,39 @@ import io.github.sceneview.rememberModelLoader
 import io.github.sceneview.rememberNodes
 import io.github.sceneview.rememberOnGestureListener
 import io.github.sceneview.rememberView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.UUID
 
 private const val TAG = "ArScreen"
 
 @Composable
 fun ARSceneComposable(
-    viewModel: ARViewModel,
     onPermissionDenied: () -> Unit
 ) {
     // LifeCycle
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
     val coroutineScope = rememberCoroutineScope()
 
     // Permission
     var hasPermission by remember { mutableStateOf(false) }
+
+    var stateString = remember { "" }
+
+    // Location
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    // Manager
+    val locationManager = remember { ARLocationManager(context, fusedLocationClient) }
+    val nodeManager = remember { ARNodeManager() }
+
+    val viewModel: ARViewModel = viewModel(factory = object : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return ARViewModel(locationManager) as T
+        }
+    })
+
+    // AR State
+    val anchorNodes by viewModel.anchorNodes.collectAsState()
+    val shouldPlaceNode by viewModel.shouldPlaceNode.collectAsState()
 
     // Dialog & SnackBar
     val showDialog by viewModel.showDialog.collectAsState()
@@ -115,27 +107,6 @@ fun ARSceneComposable(
         mutableStateOf<TrackingFailureReason?>(null)
     }
     var frame by remember { mutableStateOf<Frame?>(null) }
-
-    // AR State
-    val anchorNodes by viewModel.anchorNodes.collectAsState()
-    val npcMarkerLocations by viewModel.npcMarketNodes.collectAsState()
-
-    val currentLocation by viewModel.currentLocation.collectAsState()
-    // Location
-    var nearestNPCDistance by remember { mutableStateOf<Float?>(0f) }
-    var nearestNPC by remember { mutableStateOf<NPCLocation?>(null) }
-
-    val fusedLocationClient: FusedLocationProviderClient =
-        remember { LocationServices.getFusedLocationProviderClient(context) }
-    val locationCallback = remember {
-        object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                locationResult.lastLocation?.let { location ->
-                    viewModel.updateLocation(location)
-                }
-            }
-        }
-    }
 
     MultiplePermissionsHandler(
         permissions = listOf(
@@ -180,45 +151,7 @@ fun ARSceneComposable(
                 trackingFailureReason = it
             },
             onSessionUpdated = { session, updatedFrame ->
-                coroutineScope.launch {
-                    withContext(Dispatchers.Main) {
-                        val npcId = nearestNPC?.id ?: ""
-                        val isPlaceNPC = npcId.let { viewModel.isNPCPlaced(it) }
-
-                        if (npcId.isNotBlank() &&
-                            !isPlaceNPC &&
-                            (currentLocation?.accuracy ?: 100.0f) >= 8.0f &&
-                            (nearestNPCDistance ?: 100.0f) <= 10.0f
-                        ) {
-                            coroutineScope.launch {
-                                frame = updatedFrame
-                                updatedFrame.getUpdatedPlanes()
-                                    .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
-                                    ?.let { it.createAnchorOrNull(it.centerPose) }?.let { anchor ->
-                                        planeRenderer = false
-                                        val anchorNode = viewModel.createAnchorNode(
-                                            scriptNode[0],
-                                            engine,
-                                            modelLoader,
-                                            materialLoader,
-                                            modelInstances,
-                                            anchor,
-                                        ).apply {
-                                            val uuid = UUID.randomUUID().toString()
-
-                                            name = uuid
-
-                                            viewModel.addAnchorNode(uuid, QuestStatus.WAIT)
-
-                                            viewModel.updateNPCLocationIsPlace(npcId, true)
-                                        }
-
-                                        childNodes.add(anchorNode)
-                                    }
-                            }
-                        }
-                    }
-                }
+                frame = updatedFrame
             },
             onGestureListener = rememberOnGestureListener(
                 onSingleTapConfirmed = { motionEvent, node ->
@@ -241,13 +174,8 @@ fun ARSceneComposable(
                                             if (accepted) { // 수락
                                                 val parentAnchorNode = node.parent as? AnchorNode
                                                 parentAnchorNode?.let {
-                                                    viewModel.updateAnchorNode(
-                                                        anchorId,
-                                                        QuestStatus.PROGRESS
-                                                    )
-
                                                     coroutineScope.launch {
-                                                        viewModel.updateAnchorNode(
+                                                        nodeManager.updateAnchorNode(
                                                             prevNode = node,
                                                             questId = quest.id,
                                                             questModel = quest.model,
@@ -255,7 +183,12 @@ fun ARSceneComposable(
                                                             modelLoader = modelLoader,
                                                             materialLoader = materialLoader,
                                                             modelInstances = modelInstances,
-                                                        )
+                                                        ).apply {
+                                                            viewModel.updateQuestState(
+                                                                anchorId,
+                                                                QuestStatus.PROGRESS
+                                                            )
+                                                        }
                                                     }
                                                 }
                                             }
@@ -268,7 +201,7 @@ fun ARSceneComposable(
                                         ) { accepted ->
                                             if (accepted) { // 완료
                                                 // TODO 온디바이스 AI로 검사
-                                                viewModel.updateAnchorNode(
+                                                viewModel.updateQuestState(
                                                     anchorId,
                                                     QuestStatus.COMPLETE
                                                 )
@@ -298,28 +231,28 @@ fun ARSceneComposable(
         )
         ArStatusText(
             trackingFailureReason = trackingFailureReason,
-            childNodesEmpty = childNodes.isEmpty()
+            noneStateString = stateString
         )
 
         Column {
             Text(
-                text = "위도: ${currentLocation?.latitude ?: "모니터링중..."} ",
+                text = "위도: ${locationManager.currentLocation.value?.latitude ?: "모니터링중..."} ",
                 color = Color.White
             )
             Text(
-                text = "경도: ${currentLocation?.longitude ?: "모니터링중..."} ",
+                text = "경도: ${locationManager.currentLocation.value?.longitude ?: "모니터링중..."} ",
                 color = Color.White
             )
             Text(
-                text = "정확도: ${currentLocation?.accuracy ?: "모니터링중..."} ",
+                text = "정확도: ${locationManager.currentLocation.value?.accuracy ?: "모니터링중..."} ",
                 color = Color.Red
             )
             Text(
-                text = "가까운 마커: ${nearestNPC?.id ?: "모니터링중..."} ",
+                text = "가까운 마커: ${viewModel.nearestNPC.value?.id ?: "모니터링중..."} ",
                 color = Color.Red
             )
             Text(
-                text = "마커 거리(m): ${nearestNPCDistance ?: "모니터링중..."} ",
+                text = "마커 거리(m): ${viewModel.nearestNPCDistance.value ?: "모니터링중..."} ",
                 color = Color.Red
             )
         }
@@ -341,28 +274,25 @@ fun ARSceneComposable(
         )
     }
 
-    LaunchedEffect(currentLocation) {
-        nearestNPC = currentLocation?.let { findNearestNPC(it, npcMarkerLocations) }
-        nearestNPCDistance = currentLocation?.let { curLoc ->
-            nearestNPC?.let { curNPC ->
-                measureNearestNpcDistance(curLoc, curNPC)
-            }
-        }
-    }
+    LaunchedEffect(shouldPlaceNode) {
+        Log.d(TAG, "ARSceneComposable: $shouldPlaceNode")
+        stateString = "친구가 놀라지 않게\n주변을 천천히 둘러봐!"
 
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_RESUME) {
-                startLocationUpdates(context, fusedLocationClient, locationCallback)
-            } else if (event == Lifecycle.Event.ON_PAUSE) {
-                stopLocationUpdates(fusedLocationClient, locationCallback)
+        shouldPlaceNode?.let { npcId ->
+            nodeManager.placeNode(
+                npcId,
+                viewModel,
+                frame,
+                engine,
+                modelLoader,
+                materialLoader,
+                modelInstances,
+                childNodes
+            ).apply {
+                stateString = "클릭해서 미션을 확인해!"
+                viewModel.removeQuest(npcId)
+                viewModel.isPlacingNode = false
             }
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-            stopLocationUpdates(fusedLocationClient, locationCallback)
         }
     }
 }
@@ -370,7 +300,7 @@ fun ARSceneComposable(
 @Composable
 fun ArStatusText(
     trackingFailureReason: TrackingFailureReason?,
-    childNodesEmpty: Boolean
+    noneStateString: String
 ) {
     Text(
         modifier = Modifier
@@ -381,84 +311,14 @@ fun ArStatusText(
         fontSize = 28.sp,
         color = Color.White,
         text = (when (trackingFailureReason) {
-            TrackingFailureReason.NONE -> {
-                "위치를 찾고 있어"
-            }
-
+            TrackingFailureReason.NONE -> { "위치를 찾고 있어" }
             TrackingFailureReason.BAD_STATE -> "지금 내부 상태가\n불안정해"
             TrackingFailureReason.INSUFFICIENT_LIGHT -> "주변이 어두워서\n찾을 수 없어"
             TrackingFailureReason.EXCESSIVE_MOTION -> "너무 빨리 움직이면\n찾을 수 없어"
             TrackingFailureReason.INSUFFICIENT_FEATURES -> "주변이 막혀 있어서\n찾을 수 없어"
             TrackingFailureReason.CAMERA_UNAVAILABLE -> "카메라를 사용할 수 없어"
-            else -> {
-                if (childNodesEmpty)
-                    "동물이 놀라지 않게\n천천히 주변을 둘러봐!"
-                else
-                    "클릭해서 미션을 확인해!"
-            }
+            else -> { noneStateString }
         })
     )
 }
 
-// 위치 추적 시작
-private fun startLocationUpdates(
-    context: Context,
-    fusedLocationClient: FusedLocationProviderClient,
-    locationCallback: LocationCallback
-) {
-    val locationRequest = LocationRequest.Builder(
-        Priority.PRIORITY_HIGH_ACCURACY, 1000
-    )
-        .setWaitForAccurateLocation(false)
-        .setMinUpdateIntervalMillis(1000)
-        .setMinUpdateDistanceMeters(0.2f)
-        .build()
-
-    if (ActivityCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    ) {
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        )
-    }
-}
-
-// 위치 추적 종료
-private fun stopLocationUpdates(
-    fusedLocationClient: FusedLocationProviderClient,
-    locationCallback: LocationCallback
-) {
-    fusedLocationClient.removeLocationUpdates(locationCallback)
-}
-
-// 가장 가까운 노드와의 거리를 측정
-private fun measureNearestNpcDistance(location: Location, npcLocation: NPCLocation): Float {
-    val targetLocation = Location("target").apply {
-        latitude = npcLocation.latLng.latitude
-        longitude = npcLocation.latLng.longitude
-    }
-
-    val distanceInMeters = location.distanceTo(targetLocation)
-    return distanceInMeters
-}
-
-// 가장 가까운 노드를 찾기
-private fun findNearestNPC(
-    currentLocation: Location,
-    npcLocations: Map<String, NPCLocation>
-): NPCLocation? {
-    return npcLocations.values.minByOrNull { location ->
-        val npcLocation = Location("npc").apply {
-            latitude = location.latLng.latitude
-            longitude = location.latLng.longitude
-        }
-
-        currentLocation.distanceTo(npcLocation)
-    }
-}
-
-// viewModel에서 관리해야할 것으로 옮김
--- android/feat/ar/src/main/java/com/ssafy/ar/ARViewModel.kt
+++ android/feat/ar/src/main/java/com/ssafy/ar/ARViewModel.kt
@@ -1,44 +1,45 @@
 package com.ssafy.ar
 
 import android.location.Location
-import android.system.Os.remove
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.android.filament.Engine
-import com.google.ar.core.Anchor
-import com.ssafy.ar.ArData.CurrentLocation
-import com.ssafy.ar.ArData.NPCLocation
-import com.ssafy.ar.ArData.QuestData
-import com.ssafy.ar.ArData.QuestStatus
+import com.ssafy.ar.data.NPCLocation
+import com.ssafy.ar.data.QuestStatus
 import com.ssafy.ar.dummy.npcs
-import io.github.sceneview.ar.node.AnchorNode
-import io.github.sceneview.loaders.MaterialLoader
-import io.github.sceneview.loaders.ModelLoader
-import io.github.sceneview.math.Position
-import io.github.sceneview.math.Rotation
-import io.github.sceneview.math.Size
-import io.github.sceneview.model.ModelInstance
-import io.github.sceneview.node.ImageNode
-import io.github.sceneview.node.ModelNode
-import io.github.sceneview.node.Node
-import kotlinx.coroutines.Dispatchers
+import com.ssafy.ar.manager.ARLocationManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-private const val kMaxModelInstances = 1
+class ARViewModel(
+    private val locationManager: ARLocationManager,
+) : ViewModel() {
+    private val mutex = Mutex()
+    var isPlacingNode = false
 
-class ARViewModel : ViewModel() {
     // AR AnchorNode
     private val _anchorNodes = MutableStateFlow<Map<String, QuestStatus>>(emptyMap())
     val anchorNodes: StateFlow<Map<String, QuestStatus>> = _anchorNodes.asStateFlow()
 
-    // AR AnchorNode
+    // AR NPC
     private val _npcMarketNodes = MutableStateFlow<Map<String, NPCLocation>>(emptyMap())
     val npcMarketNodes: StateFlow<Map<String, NPCLocation>> = _npcMarketNodes.asStateFlow()
+
+    // 가장 가까운 NPC
+    private val _nearestNPC = MutableStateFlow<NPCLocation?>(null)
+    val nearestNPC: StateFlow<NPCLocation?> = _nearestNPC.asStateFlow()
+
+    // 가장 가까운 NPC와의 거리(m)
+    private val _nearestNPCDistance = MutableStateFlow<Float?>(0f)
+    val nearestNPCDistance: StateFlow<Float?> = _nearestNPCDistance.asStateFlow()
+
+    // 배치 가능한 노드
+    private val _shouldPlaceNode = MutableStateFlow<String?>(null)
+    val shouldPlaceNode: StateFlow<String?> = _shouldPlaceNode.asStateFlow()
 
     // Dialog
     private val _showDialog = MutableStateFlow(false)
@@ -49,23 +50,22 @@ class ARViewModel : ViewModel() {
     val dialogData: StateFlow<Pair<Int, QuestStatus> > = _dialogData
     private var dialogCallback: ((Boolean) -> Unit)? = null
 
-    private val _currentLocation = MutableStateFlow<Location?>(null)
-    val currentLocation = _currentLocation.asStateFlow()
-
-    fun updateLocation(location: Location) {
+    init {
         viewModelScope.launch {
-            _currentLocation.emit(location)
+            locationManager.startLocationUpdates().collect { location ->
+                updateNearestNPC(location)
+            }
         }
     }
 
-    fun addAnchorNode(id: String, state: QuestStatus) {
+    fun addQuest(id: String, state: QuestStatus) {
         val updatedMap = _anchorNodes.value.toMutableMap().apply {
             put(id, state)
         }
         _anchorNodes.value = updatedMap
     }
 
-    fun updateAnchorNode(id: String, newState: QuestStatus) {
+    fun updateQuestState(id: String, newState: QuestStatus) {
         viewModelScope.launch {
             val updatedMap = _anchorNodes.value.toMutableMap().apply {
                 this[id] = newState
@@ -74,7 +74,7 @@ class ARViewModel : ViewModel() {
         }
     }
 
-    fun removeAnchorNode(id: String) {
+    fun removeQuest(id: String) {
         viewModelScope.launch {
             val updatedMap = _anchorNodes.value.toMutableMap().apply {
                 remove(id)
@@ -83,7 +83,7 @@ class ARViewModel : ViewModel() {
         }
     }
 
-    fun getAnchorNodeId(id: String): QuestStatus? {
+    fun getQuestState(id: String): QuestStatus? {
         return _anchorNodes.value[id]
     }
 
@@ -91,7 +91,7 @@ class ARViewModel : ViewModel() {
         _npcMarketNodes.value = npcs
     }
 
-    fun updateNPCLocationIsPlace(id: String, newIsPlaceValue: Boolean) {
+    fun updateIsPlaceNPC(id: String, newIsPlaceValue: Boolean) {
         _npcMarketNodes.update { currentMap ->
             currentMap.toMutableMap().apply {
                 this[id]?.let { npcLocation ->
@@ -101,8 +101,41 @@ class ARViewModel : ViewModel() {
         }
     }
 
-    fun isNPCPlaced(id: String): Boolean {
+    private fun getIsPlaceNPC(id: String): Boolean {
         return _npcMarketNodes.value[id]?.isPlace ?: false
+    }
+
+    private fun updateNearestNPC(location: Location) {
+        viewModelScope.launch {
+            val nearestNPC = locationManager.findNearestNPC(location, npcMarketNodes.value)
+            _nearestNPC.value = nearestNPC
+
+            val nearestDistance =
+                nearestNPC?.let { locationManager.measureNearestNpcDistance(location, it) }
+
+            _nearestNPCDistance.value = nearestDistance
+
+            processLocation()
+        }
+    }
+
+    private suspend fun processLocation() {
+        mutex.withLock {
+            if (isPlacingNode) return
+
+            isPlacingNode = true
+
+            val npcId = nearestNPC.value?.id ?: ""
+            val isPlaceNPC = getIsPlaceNPC(npcId)
+
+            if (npcId.isNotBlank() &&
+                !isPlaceNPC &&
+                (locationManager.currentLocation.value?.accuracy ?: 100.0f) <= 10.0f &&
+                (nearestNPCDistance.value ?: 100f) <= 10.0f
+            ) {
+                _shouldPlaceNode.value = npcId
+            }
+        }
     }
 
     fun showQuestDialog(index: Int, state: QuestStatus, callback: (Boolean) -> Unit) {
@@ -125,88 +158,9 @@ class ARViewModel : ViewModel() {
         dialogCallback = null
     }
 
-    suspend fun updateAnchorNode(
-        prevNode: Node,
-        questId: String,
-        questModel: String,
-        parentAnchor: AnchorNode,
-        modelLoader: ModelLoader,
-        materialLoader: MaterialLoader,
-        modelInstances: MutableMap<String, ModelInstance>,
-    ) = withContext(Dispatchers.Main) {
-        parentAnchor.removeChildNode(prevNode).also {
-            val newModelInstance = modelInstances.getOrPut(questId) {
-                modelLoader.createInstancedModel(
-                    questModel,
-                    kMaxModelInstances
-                ).first()
-            }
+    override fun onCleared() {
+        super.onCleared()
 
-            val newModelNode = ModelNode(
-                modelInstance = newModelInstance,
-                scaleToUnits = 0.5f
-            ).apply {
-                name = questId
-                position = prevNode.worldPosition
-                rotation = prevNode.worldRotation
-            }
-
-            val imageNode = ImageNode(
-                materialLoader = materialLoader,
-                imageFileLocation = "picture/progress.png",
-                size = Size(0.35f, 0.35f),
-                center = Position(0f, 0.67f, 0f)
-            )
-
-            newModelNode.addChildNode(imageNode)
-
-            parentAnchor.addChildNode(newModelNode)
-        }
-    }
-
-    // 특정 위치에 3D 모델을 배치
-    suspend fun createAnchorNode(
-        node: QuestData,
-        engine: Engine,
-        modelLoader: ModelLoader,
-        materialLoader: MaterialLoader,
-        modelInstances: MutableMap<String, ModelInstance>,
-        anchor: Anchor
-    ): AnchorNode {
-        val idx = (1..4).random()
-
-        val anchorNode = AnchorNode(engine = engine, anchor = anchor).apply {
-            isPositionEditable = false
-            isRotationEditable = false
-            isScaleEditable = false
-        }
-
-        val modelInstance = modelInstances.getOrPut(node.id) {
-            modelLoader.createInstancedModel(
-                node.model,
-                kMaxModelInstances
-            ).first()
-        }
-
-        val modelNode = ModelNode(
-            modelInstance = modelInstance,
-            scaleToUnits = 0.5f
-        ).apply {
-            name = idx.toString()
-            rotation = Rotation(0f, 180f, 0f)
-        }
-
-        val imageNode = ImageNode(
-            materialLoader = materialLoader,
-            imageFileLocation = "picture/wait.png",
-            size = Size(0.35f, 0.35f),
-            center = Position(0f, 0.65f, 0f)
-        )
-
-        modelNode.addChildNode(imageNode)
-
-        anchorNode.addChildNode(modelNode)
-
-        return anchorNode
+        locationManager.stopLocationUpdates()
     }
 }
