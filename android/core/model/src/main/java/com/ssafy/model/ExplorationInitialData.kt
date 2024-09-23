cd1be4b71626cf33a450fd172f3adb60021061d4
김성수 <skele1207@gmail.com>
2024-09-23 17:49:14 +0900

    feat : model serialization 적용

--- android/core/model/src/main/java/com/ssafy/model/ExplorationInitialData.kt
+++ android/core/model/src/main/java/com/ssafy/model/ExplorationInitialData.kt
@@ -1,5 +1,9 @@
 package com.ssafy.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ExplorationData(
     val id: Long,
     val myDiscoveredSpecies: List<Species>,
@@ -7,14 +11,15 @@ data class ExplorationData(
     val npcs: List<Npc>,
     val completedQuest: Int,
 )
-
+@Serializable
 data class ExplorationInitialData(
+    @SerialName("explorationId")
     val id: Long,
     val myDiscoveredSpecies: List<Species>,
     val species: List<Species>,
     val npcs: List<Npc>,
 )
-
+@Serializable
 data class ExplorationEndData(
     val explorationId: Long,
 )
