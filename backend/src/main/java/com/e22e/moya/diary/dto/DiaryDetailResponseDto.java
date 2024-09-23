29469d211b854e3fef1a1eef4573863047d02b3c
강미연 <422haha@naver.com>
2024-09-23 12:35:20 +0900

    S11P21D202-372 feat : 탐험일지 DTO 작성

--- backend/src/main/java/com/e22e/moya/diary/dto/DiaryDetailResponseDto.java
+++ backend/src/main/java/com/e22e/moya/diary/dto/DiaryDetailResponseDto.java
@@ -1,5 +1,21 @@
 package com.e22e.moya.diary.dto;
 
-public class DiaryDetailResponseDto {
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class DiaryDetailResponseDto {
+    private Long explorationId;
+    private String parkName;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private double distance;
+    private int steps;
+    private List<CollectedSpeciesDto> collectedSpecies;
+    private List<String> movementPath;
 }
