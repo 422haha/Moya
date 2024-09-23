b160e5aabc5c41e737f0f6898b31b8e5da7c5846
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-09-23 17:45:56 +0900

    feat: 테스트 좌표 추가

--- android/feat/main/src/main/java/com/ssafy/main/explorestart/ExploreStartScreenViewModel.kt
+++ android/feat/main/src/main/java/com/ssafy/main/explorestart/ExploreStartScreenViewModel.kt
@@ -1,17 +1,30 @@
 package com.ssafy.main.explorestart
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.naver.maps.geometry.LatLng
 import com.ssafy.ui.explorestart.ExploreStartScreenState
 import com.ssafy.ui.explorestart.ExploreStartUserIntent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ExploreStartScreenViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow<ExploreStartScreenState>(ExploreStartScreenState.Loading)
     val state: StateFlow<ExploreStartScreenState> = _state
+
+    init {
+        viewModelScope.launch {
+            val markerPositions = listOf(
+                LatLng(37.5665, 126.9780),
+                LatLng(35.1796, 129.0756)
+            )
+            _state.value = ExploreStartScreenState.Loaded(markerPositions = markerPositions)
+        }
+    }
 
     fun onIntent(intent: ExploreStartUserIntent) {
 
