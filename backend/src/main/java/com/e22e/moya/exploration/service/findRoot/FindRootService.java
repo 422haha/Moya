2bd2d334e354dcf9aec7fa60ca7bb324e24258b0
서장원 <seojang980510@naver.com>
2024-09-24 14:44:50 +0900

    fix : 빈 데이터 new Object[]{}로

---
+++ backend/src/main/java/com/e22e/moya/exploration/service/findRoot/FindRootService.java
@@ -0,0 +1,5 @@
+package com.e22e.moya.exploration.service.findRoot;
+
+public interface FindRootService {
+
+}
---
+++ backend/src/main/java/com/e22e/moya/exploration/service/findRoot/FindRootServiceImpl.java
@@ -0,0 +1,5 @@
+package com.e22e.moya.exploration.service.findRoot;
+
+public class FindRootServiceImpl implements FindRootService {
+
+}
