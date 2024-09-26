eaaa3e9285cca5c50b77295ddb1accfdd9ac271f
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-09-26 15:03:30 +0900

    feat: progress 영역 수정

--- android/core/ui/src/main/java/com/ssafy/ui/encyclopedia/EncycScreenContent.kt
+++ android/core/ui/src/main/java/com/ssafy/ui/encyclopedia/EncycScreenContent.kt
@@ -1,7 +1,6 @@
 package com.ssafy.ui.encyclopedia
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
@@ -22,12 +20,14 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,12 +35,12 @@ import androidx.compose.ui.unit.sp
 import com.ssafy.ui.component.ErrorScreen
 import com.ssafy.ui.component.LoadingScreen
 import com.ssafy.ui.component.PlantCard
+import com.ssafy.ui.component.PlantInfo
 import com.ssafy.ui.component.TopBar
-import com.ssafy.ui.component.plantInfo
+import com.ssafy.ui.theme.GrayColor
 import com.ssafy.ui.theme.LightBackgroundColor
 import com.ssafy.ui.theme.PrimaryColor
-import com.ssafy.ui.theme.StarYellowColor
-import com.ssafy.ui.theme.SurfaceColor
+import com.ssafy.ui.theme.customTypography
 
 val chipLabels = listOf("전체", "수집완료", "미발견")
 
@@ -48,11 +48,11 @@ val chipLabels = listOf("전체", "수집완료", "미발견")
 fun EncycScreenContent(
     modifier: Modifier = Modifier,
     encycScreenState: EncycScreenState,
-    onIntent: (EncycUserIntent) -> Unit = {}
+    onIntent: (EncycUserIntent) -> Unit = {},
 ) {
     Scaffold(
         topBar = {
-            TopBar("도감", PrimaryColor, onPop = { onIntent(EncycUserIntent.OnPop) })
+            TopBar("도감", onPop = { onIntent(EncycUserIntent.OnPop) })
         },
         content = { paddingValues ->
             when (encycScreenState) {
@@ -64,15 +64,18 @@ fun EncycScreenContent(
                     EncycScreenLoaded(
                         modifier = modifier.padding(paddingValues),
                         state = encycScreenState,
-                        onIntent = onIntent
+                        onIntent = onIntent,
                     )
                 }
 
                 is EncycScreenState.Error -> {
-                    ErrorScreen(modifier = modifier.padding(paddingValues), encycScreenState.message)
+                    ErrorScreen(
+                        modifier = modifier.padding(paddingValues),
+                        encycScreenState.message,
+                    )
                 }
             }
-        }
+        },
     )
 }
 
@@ -80,64 +83,73 @@ fun EncycScreenContent(
 fun EncycScreenLoaded(
     modifier: Modifier,
     state: EncycScreenState.Loaded,
-    onIntent: (EncycUserIntent) -> Unit = {}
+    onIntent: (EncycUserIntent) -> Unit = {},
 ) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
+        modifier =
+            modifier
+                .fillMaxSize(),
     ) {
+        CollectionProgress(progress = state.progress)
         FilterChips(
             selectedChipIndex = state.selectedChipIndex,
             onChipSelected = { index ->
                 onIntent(EncycUserIntent.OnChipSelected(index))
-            })
+            },
+        )
         EncycGrid(
             items = state.items,
             modifier = Modifier.weight(1f),
-            onItemClicked = { onIntent(EncycUserIntent.OnItemSelect(it)) }
+            onItemClicked = { onIntent(EncycUserIntent.OnItemSelect(it)) },
         )
-        CollectionProgress(progress = state.progress)
     }
 }
 
 @Composable
-fun FilterChipComponent(text: String, isSelected: Boolean, onClick: () -> Unit) {
+fun FilterChipComponent(
+    text: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+) {
     FilterChip(
         selected = isSelected,
         onClick = onClick,
         label = {
             Text(
                 text = text,
-                color = if (isSelected) LightBackgroundColor else PrimaryColor,
-                fontWeight = FontWeight.Bold
+                color = if (isSelected) LightBackgroundColor else Color.Black,
+                fontWeight = FontWeight.Bold,
             )
         },
         enabled = true,
         shape = RoundedCornerShape(16.dp),
-        colors = FilterChipDefaults.filterChipColors(
-            containerColor = if (isSelected) PrimaryColor else SurfaceColor,
-            selectedContainerColor = PrimaryColor,
-            labelColor = if (isSelected) LightBackgroundColor else PrimaryColor
-        ),
-        border = if (!isSelected) {
-            BorderStroke(1.dp, PrimaryColor)
-        } else {
-            null
-        }
+        colors =
+            FilterChipDefaults.filterChipColors(
+                containerColor = if (isSelected) PrimaryColor else LightBackgroundColor,
+                selectedContainerColor = PrimaryColor,
+                labelColor = if (isSelected) LightBackgroundColor else Color.Black,
+            ),
+        border =
+            if (!isSelected) {
+                BorderStroke(1.dp, Color.Black)
+            } else {
+                null
+            },
     )
 }
 
 @Composable
 fun FilterChips(
     selectedChipIndex: Int,
-    onChipSelected: (Int) -> Unit
+    onChipSelected: (Int) -> Unit,
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .padding(top = 8.dp)
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(top = 8.dp),
     ) {
         chipLabels.forEachIndexed { index, label ->
             FilterChipComponent(
@@ -145,7 +157,7 @@ fun FilterChips(
                 isSelected = index == selectedChipIndex,
                 onClick = {
                     onChipSelected(index)
-                }
+                },
             )
         }
     }
@@ -155,31 +167,31 @@ fun FilterChips(
 data class EncycGridState(
     val plantName: String,
     val plantImage: String?,
-    val isDiscovered: Boolean
+    val isDiscovered: Boolean,
 )
 
 @Composable
 fun EncycGrid(
     items: List<EncycGridState>,
     modifier: Modifier = Modifier,
-    onItemClicked: (Long) -> Unit
+    onItemClicked: (Long) -> Unit,
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(4.dp),
-        modifier = modifier
-            .fillMaxHeight()
-            .heightIn(min = 200.dp)
-            .padding(horizontal = 8.dp)
+        modifier =
+            modifier
+                .fillMaxHeight()
+                .heightIn(min = 200.dp),
     ) {
         itemsIndexed(items) { index, item ->
             PlantCard(
-                plantInfo(
+                PlantInfo(
                     plantName = item.plantName,
                     plantImage = item.plantImage,
-                    isDiscovered = item.isDiscovered
+                    isDiscovered = item.isDiscovered,
                 ),
-                onClick = { onItemClicked(index.toLong()) }
+                onClick = { onItemClicked(index.toLong()) },
             )
         }
     }
@@ -187,43 +199,47 @@ fun EncycGrid(
 
 @Composable
 fun CollectionProgress(progress: Float) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(40.dp)
-            .background(PrimaryColor)
-    ) {
-        Text(
-            text = "수집률",
-            fontSize = 16.sp,
-            color = LightBackgroundColor,
-            modifier = Modifier.padding(start = 8.dp)
-        )
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp)
+    Surface(shadowElevation = 8.dp) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier =
+                Modifier
+                    .fillMaxWidth(),
         ) {
-            LinearProgressIndicator(
-                progress = { progress / 100 },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(8.dp)
-                    .clip(RoundedCornerShape(4.dp)),
-                color = StarYellowColor,
+            Text(
+                text = "수집률",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                style = customTypography.titleMedium,
+                modifier =
+                    Modifier
+                        .padding(start = 8.dp)
+                        .padding(top = 8.dp),
             )
 
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
             Text(
                 text = "$progress%",
-                fontWeight = FontWeight.Bold,
-                color = LightBackgroundColor
+                color = Color.Gray,
+                modifier =
+                    Modifier
+                        .align(Alignment.End)
+                        .padding(end = 8.dp),
             )
+
+            LinearProgressIndicator(
+                progress = { progress / 100 },
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .height(8.dp)
+                        .padding(horizontal = 8.dp)
+                        .clip(RoundedCornerShape(4.dp)),
+                color = PrimaryColor,
+                trackColor = GrayColor,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
@@ -232,17 +248,19 @@ fun CollectionProgress(progress: Float) {
 @Composable
 fun EncycScreenPreview() {
     EncycScreenContent(
-        encycScreenState = EncycScreenState.Loaded(
-            selectedChipIndex = 0,
-            items = List(8) { index ->
-                EncycGridState(
-                    plantName = "식물 $index",
-                    plantImage = null,
-                    isDiscovered = index % 2 == 0
-                )
-            },
-            progress = 60.0f
-        )
+        encycScreenState =
+            EncycScreenState.Loaded(
+                selectedChipIndex = 0,
+                items =
+                    List(8) { index ->
+                        EncycGridState(
+                            plantName = "식물 $index",
+                            plantImage = null,
+                            isDiscovered = index % 2 == 0,
+                        )
+                    },
+                progress = 60.0f,
+            ),
     )
 }
 
