a216aaf19c4d88eced9149f565175b1e0de3900e
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-09-26 14:56:16 +0900

    feat: plantcard를 변경

--- android/core/ui/src/main/java/com/ssafy/ui/component/EncycCard.kt
+++ android/core/ui/src/main/java/com/ssafy/ui/component/EncycCard.kt
@@ -3,24 +3,23 @@ package com.ssafy.ui.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,62 +27,61 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.ssafy.ui.R
-import com.ssafy.ui.theme.DarkGrayColor
-import com.ssafy.ui.theme.GrayColor
 import com.ssafy.ui.theme.LightBackgroundColor
-import com.ssafy.ui.theme.PrimaryColor
 import com.ssafy.ui.theme.SurfaceColor
 
 @Immutable
-data class plantInfo(
+data class PlantInfo(
     val plantName: String,
     val plantImage: String?,
     val isDiscovered: Boolean
 )
 
 @Composable
-fun PlantCard(plantInfo: plantInfo, onClick: () -> Unit = {}) {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(4.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = if (plantInfo.isDiscovered) SurfaceColor else GrayColor
-        ),
+fun PlantCard(plantInfo: PlantInfo, onClick: () -> Unit = {}) {
+    Box(
         modifier = Modifier
-            .padding(4.dp)
-            .width(160.dp)
-            .height(160.dp),
-        onClick = onClick
+            .padding(8.dp)
+            .size(160.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .clickable { onClick() }
     ) {
-        Column(
+        AsyncImage(
+            model = plantInfo.plantImage,
+            contentDescription = "도감 사진",
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+                .fillMaxWidth()
+                .background(LightBackgroundColor),
+            placeholder = painterResource(id = R.drawable.ic_launcher_background)
+        )
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            color = if (plantInfo.isDiscovered) SurfaceColor.copy(alpha = 0.5f)
+            else Color.Black.copy(alpha = 0.5f)
         ) {
-            AsyncImage(
-                model = plantInfo.plantImage,
-                contentDescription = "도감 사진",
-                contentScale = ContentScale.Crop,
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(120.dp)
-                    .background(LightBackgroundColor)
-            )
-
-            Text(
-                text = plantInfo.plantName,
-                fontSize = 14.sp,
-                color = if (plantInfo.isDiscovered) PrimaryColor else DarkGrayColor,
-                modifier = Modifier
                     .padding(8.dp),
-            )
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = plantInfo.plantName,
+                    fontSize = 14.sp,
+                    color = if (plantInfo.isDiscovered) Color.Black else LightBackgroundColor
+                )
+            }
         }
     }
+
 }
 
 @Composable
-fun CirclePlantComponent(plantInfo: plantInfo, onClick: () -> Unit = {}) {
+fun CirclePlantComponent(plantInfo: PlantInfo, onClick: () -> Unit = {}) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -109,7 +107,7 @@ fun CirclePlantComponent(plantInfo: plantInfo, onClick: () -> Unit = {}) {
 @Composable
 fun PlantCardPreview() {
     PlantCard(
-        plantInfo(
+        PlantInfo(
             plantName = "능소화",
             plantImage = "",
             isDiscovered = true
@@ -121,7 +119,7 @@ fun PlantCardPreview() {
 @Composable
 fun PlantCardWithFalsePreview() {
     PlantCard(
-        plantInfo(
+        PlantInfo(
             plantName = "능소화",
             plantImage = "",
             isDiscovered = false
@@ -133,7 +131,7 @@ fun PlantCardWithFalsePreview() {
 @Composable
 fun CirclePlantComponentPreview() {
     CirclePlantComponent(
-        plantInfo(
+        PlantInfo(
             plantName = "능소화",
             plantImage = "",
             isDiscovered = false
