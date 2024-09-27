5efd7083ebedfaee2cd0311c18b6bbfc902bcc0b
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-09-27 13:46:04 +0900

    feat: id 추가

--- android/core/ui/src/main/java/com/ssafy/ui/explorestart/ExploreStartScreenState.kt
+++ android/core/ui/src/main/java/com/ssafy/ui/explorestart/ExploreStartScreenState.kt
@@ -6,19 +6,25 @@ import com.naver.maps.geometry.LatLng
 sealed interface ExploreStartScreenState {
     @Immutable
     data object Loading : ExploreStartScreenState
+
     @Immutable
     data class Loaded(
         val npcPositions: List<LatLng> = listOf(),
         val discoveredPositions: List<LatLng> = listOf(),
         val speciesPositions: List<LatLng> = listOf(),
         val showExitDialog: Boolean = false,
-        val showChallengeDialog: Boolean = false
+        val showChallengeDialog: Boolean = false,
     ) : ExploreStartScreenState
+
     @Immutable
-    data class Error(val message: String) : ExploreStartScreenState
+    data class Error(
+        val message: String,
+    ) : ExploreStartScreenState
 }
 
+@Immutable
 data class Missions(
+    val id: Long,
     val missionTitle: String,
-    val isSuccess: Boolean
+    val isSuccess: Boolean,
 )
