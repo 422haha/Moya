1bffb98da9e8b6bded21b9dfc6d3db269302eedf
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-09-27 10:14:35 +0900

    feat: API 호출 함수 작성

--- android/feat/main/src/main/java/com/ssafy/main/encyclopedia/EncycScreenViewModel.kt
+++ android/feat/main/src/main/java/com/ssafy/main/encyclopedia/EncycScreenViewModel.kt
@@ -1,19 +1,57 @@
 package com.ssafy.main.encyclopedia
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.network.ApiResponse
+import com.ssafy.network.repository.EncyclopediaRepository
+import com.ssafy.ui.encyclopedia.EncycGridState
 import com.ssafy.ui.encyclopedia.EncycScreenState
 import com.ssafy.ui.encyclopedia.EncycUserIntent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EncycScreenViewModel @Inject constructor(): ViewModel() {
-    private val _state = MutableStateFlow<EncycScreenState>(EncycScreenState.Loading)
-    val state: StateFlow<EncycScreenState> = _state
+class EncycScreenViewModel
+    @Inject
+    constructor(
+        private val encyclopediaRepository: EncyclopediaRepository,
+    ) : ViewModel() {
+        private val _state = MutableStateFlow<EncycScreenState>(EncycScreenState.Loading)
+        val state: StateFlow<EncycScreenState> = _state
 
-    fun onIntent(intent: EncycUserIntent) {
+        fun loadInitialData(parkId: Long) {
+            viewModelScope.launch {
+                encyclopediaRepository.getEncyclopedia(parkId).collectLatest { response ->
+                    _state.value =
+                        when (response) {
+                            is ApiResponse.Success -> {
+                                response.body?.let { body ->
+                                    EncycScreenState.Loaded(
+                                        items =
+                                            body.data.species.map {
+                                                EncycGridState(
+                                                    plantName = it.speciesName,
+                                                    plantImage = it.imageUrl,
+                                                    isDiscovered = it.discovered,
+                                                )
+                                            },
+                                        progress = body.data.progress,
+                                    )
+                                } ?: EncycScreenState.Error("Failed to load initial data")
+                            }
 
+                            is ApiResponse.Error -> {
+                                EncycScreenState.Error(response.errorMessage ?: "Unknown error")
+                            }
+                        }
+                }
+            }
+        }
+
+        fun onIntent(intent: EncycUserIntent) {
+        }
     }
-}
