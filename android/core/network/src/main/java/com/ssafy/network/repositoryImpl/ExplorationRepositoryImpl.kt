21a9126cad7d6beca116bfece2ca850d65544753
김성수 <skele1207@gmail.com>
2024-09-30 18:01:24 +0900

    feat: exploration api 변경

--- android/core/network/src/main/java/com/ssafy/network/repositoryImpl/ExplorationRepositoryImpl.kt
+++ android/core/network/src/main/java/com/ssafy/network/repositoryImpl/ExplorationRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.ssafy.network.repositoryImpl
 import com.ssafy.model.CompletedQuest
 import com.ssafy.model.ExplorationEndData
 import com.ssafy.model.ExplorationInitialData
-import com.ssafy.model.ExplorationJournalRecent
 import com.ssafy.model.QuestList
 import com.ssafy.model.SpeciesMinimumInfo
 import com.ssafy.network.ApiResponse
@@ -41,25 +40,56 @@ class ExplorationRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun getRecentExploration(
-        explorationId: Long,
-        body: ExplorationEndRequestBody
-    ): Flow<ApiResponse<ExplorationJournalRecent>> {
-        TODO("Not yet implemented")
-    }
-
     override suspend fun endExploration(
         explorationId: Long,
         body: ExplorationEndRequestBody
     ): Flow<ApiResponse<ExplorationEndData>> {
-        TODO("Not yet implemented")
+        return flow {
+            val response =
+                apiHandler {
+                    explorationApi.endExploration(explorationId, body)
+                }
+            when (response) {
+                is ApiResponse.Success -> {
+                    emit(ApiResponse.Success(response.body?.data))
+                }
+
+                is ApiResponse.Error -> {
+                    emit(
+                        ApiResponse.Error(
+                            errorCode = response.errorCode,
+                            errorMessage = response.errorMessage,
+                        ),
+                    )
+                }
+            }
+        }
     }
 
     override suspend fun registerSpecies(
         explorationId: Long,
         body: ExplorationEndRequestBody
     ): Flow<ApiResponse<SpeciesMinimumInfo>> {
-        TODO("Not yet implemented")
+        return flow {
+            val response =
+                apiHandler {
+                    explorationApi.registerSpecies(explorationId, body)
+                }
+            when (response) {
+                is ApiResponse.Success -> {
+                    emit(ApiResponse.Success(response.body?.data))
+                }
+
+                is ApiResponse.Error -> {
+                    emit(
+                        ApiResponse.Error(
+                            errorCode = response.errorCode,
+                            errorMessage = response.errorMessage,
+                        ),
+                    )
+                }
+            }
+        }
     }
 
     override suspend fun getExplorationData(explorationId: Long): Flow<ApiResponse<ExplorationEndData>> {
