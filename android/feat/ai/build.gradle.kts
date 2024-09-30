45eb9094ed248f3b72165ec6bf2ad759a4eab5fd
김성수 <skele1207@gmail.com>
2024-09-30 15:43:16 +0900

    feat: ai module 추가

---
+++ android/feat/ai/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.module.android.library.compose)
+    alias(libs.plugins.module.hilt)
+}
+
+android {
+    namespace = "com.ssafy.moya.ai"
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+}
+
+dependencies {
+    implementation(libs.onnxruntime)
+}
