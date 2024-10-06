4df1036b3235db21c63a9bdf0639eac44e2081e7
SeungSoo <skele1207@gmail.com>
2024-10-06 22:12:40 +0900

    feature : testing code 추가

--- android/feat/main/src/main/java/com/ssafy/main/login/LoginScreen.kt
+++ android/feat/main/src/main/java/com/ssafy/main/login/LoginScreen.kt
@@ -1,7 +1,11 @@
 package com.ssafy.main.login
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ssafy.ui.login.LoginScreenContent
@@ -12,6 +16,17 @@ fun LoginScreen(
     viewModel: LoginScreenViewModel = hiltViewModel(),
     onNavigateToHome: () -> Unit,
 ) {
+    val context = LocalContext.current
+    val upload = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { result ->
+        result?.let {
+            viewModel.upload(context, it)  
+        } 
+    }
+    
+    LaunchedEffect(key1 = Unit) {
+        upload.launch("image/*")
+    }
+
     val uiState by viewModel.state.collectAsStateWithLifecycle()
     LoginScreenContent(loginScreenState = uiState, onIntent = { intent ->
         when (intent) {
--- android/feat/main/src/main/java/com/ssafy/main/login/LoginScreenViewModel.kt
+++ android/feat/main/src/main/java/com/ssafy/main/login/LoginScreenViewModel.kt
@@ -1,19 +1,82 @@
 package com.ssafy.main.login
 
+import android.content.Context
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.network.ApiResponse
+import com.ssafy.network.repository.UploadRepository
 import com.ssafy.ui.login.LoginScreenState
 import com.ssafy.ui.login.LoginUserIntent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginScreenViewModel @Inject constructor() : ViewModel() {
+class LoginScreenViewModel @Inject constructor(
+    private val uploadRepository: UploadRepository
+) : ViewModel() {
     private val _state = MutableStateFlow<LoginScreenState>(LoginScreenState.Loading)
     val state: StateFlow<LoginScreenState> = _state
 
     fun onIntent(intent: LoginUserIntent) {
 
+    }
+
+    fun upload(context: Context, uri: Uri){
+        getFileFromUri(context, uri)?.let {
+            viewModelScope.launch {
+                uploadRepository.upload("test_image.jpg", it).collectLatest { response ->
+                    when(response){
+                        is ApiResponse.Error -> {
+                            Log.d("TAG", "upload: ${response.errorMessage}")
+                        }
+                        is ApiResponse.Success -> {
+                            Log.d("TAG", "upload: ${response.body}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun getFileFromUri(context: Context, uri: Uri): File? {
+        val fileName = getFileName(context, uri) ?: return null
+        val cacheDir = context.cacheDir
+        val file = File(cacheDir,fileName)
+
+        try {
+            context.contentResolver.openInputStream(uri)?.use { inputStream ->
+                FileOutputStream(file).use { outputStream ->
+                    inputStream.copyTo(outputStream)
+                }
+            }
+            return file
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    fun getFileName(context: Context, uri: Uri): String? {
+        val cursor = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val displayNameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (displayNameIndex != -1) {
+                    return it.getString(displayNameIndex)
+                }
+            }
+        }
+        return null
     }
 }
